c085c2145359910013b6c79e24e2a2866498b96a
ipan irtiano <ipanirtiano@gmail.com>
2024-03-14 21:41:07 +0700

    update adapter 2

--- app/utils/authOptions.ts
+++ app/utils/authOptions.ts
@@ -5,6 +5,7 @@ import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { Adapter } from "next-auth/adapters";
 
+
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma) as Adapter,
     secret: process.env.NEXTAUTH_SECRET,
