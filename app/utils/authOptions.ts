47a1b47d2d3e30b7cea89a4905702fd35b3fae5f
ipan irtiano <ipanirtiano@gmail.com>
2024-03-14 21:36:28 +0700

    update adapter auth options

--- app/utils/authOptions.ts
+++ app/utils/authOptions.ts
@@ -3,18 +3,19 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/prisma/prisma_db";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { Adapter } from "next-auth/adapters";
 
 export const authOptions: NextAuthOptions = {
-adapter: PrismaAdapter(prisma),
-secret: process.env.NEXTAUTH_SECRET,
-providers: [
-    GitHubProvider({
-        clientId: process.env.GITHUB_ID as string,
-        clientSecret: process.env.GITHUB_SECRET as string
+    adapter: PrismaAdapter(prisma) as Adapter,
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
         }),
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         })
-]
+    ]
 }
